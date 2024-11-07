e6dceabf3598d7f254574e6ae1b3fe9e08867a6b
Code-X-Rahul <rr611305@gmail.com>
2024-11-07 15:35:03 +0530

    project initialised and folder structure setup

---
+++ src/app/(protected pages)/ledger/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const LedgerPage = (props: Props) => {
+  return <div>LedgerPage</div>;
+};
+
+export default LedgerPage;
---
+++ src/app/(protected pages)/stock-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const StockInPage = (props: Props) => {
+  return (
+    <div>StockInPage</div>
+  )
+}
+
+export default StockInPage
