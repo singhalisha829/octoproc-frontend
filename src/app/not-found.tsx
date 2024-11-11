5390e10427fbc9ac25ed7774e560fe9893f984d7
Code-X-Rahul <rr611305@gmail.com>
2024-11-11 11:34:48 +0530

    not-found page added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex items-center justify-center text-4xl font-medium uppercase h-full">
+      oops.. the page that you were looking for is not available.
+    </div>
+  );
+};
+
+export default NotFoundPage;
