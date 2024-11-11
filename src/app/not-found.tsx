073801cb403545436d71670583b138ef22c0c25d
Code-X-Rahul <rr611305@gmail.com>
2024-11-11 13:45:49 +0530

    not found text color changed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex items-center justify-center text-4xl font-medium uppercase h-full">
+    <div className="flex items-center justify-center text-4xl font-medium uppercase h-full text-white">
       oops.. the page that you were looking for is not available.
     </div>
   );
