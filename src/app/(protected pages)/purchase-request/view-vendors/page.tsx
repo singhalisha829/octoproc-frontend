b257498a694b59ca979e820712cc3ae29e3cce34
Code-X-Rahul <rr611305@gmail.com>
2024-11-12 10:51:15 +0530

    view vendors page ui started

---
+++ src/app/(protected pages)/purchase-request/view-vendors/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+import Container from "@/components/globals/Container";
+import Header from "@/components/globals/Header";
+import { DataTable } from "@/components/table/data-table";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { PurchaseRequest } from "../purchase-table-columns";
+import { vendorColumns } from "./view-vendor-columns";
+
+const ViewVendorsPage = () => {
+  const [purchaseRequest, setPurchaseRequest] = useState<PurchaseRequest>({
+    id: 1,
+    name: "Demo purchase request",
+    items: [
+      {
+        unitPrice: 20,
+        quantity: 20,
+        partName: "item",
+        partId: 2,
+      },
+    ],
+    vendors: [{ id: 1, name: "Demo Vendor", quantity: 20 }],
+  });
+  return (
+    <>
+      <Header title="View Assigned Vendors" description="" />
+
+      {purchaseRequest.vendors.map((vendor) => (
+        <Container className="grid gap-4 " key={vendor.id}>
+          <div className="flex items-center justify-between">
+            <p className="font-bold text-lg">{vendor.name}</p>
+            <div className="flex items-center gap-2">
+              <Button variant={"tertiary"}>
+                {/* <Link href={"/#"}> */}
+                Create Purchase Request
+                {/* </Link> */}
+              </Button>
+            </div>
+          </div>
+          <DataTable data={purchaseRequest.items} columns={vendorColumns} />
+        </Container>
+      ))}
+    </>
+  );
+};
+
+export default ViewVendorsPage;
