2f4b45fa85e63697bfd83fbfbf8b30873db0fea4
Code-X-Rahul <rr611305@gmail.com>
2024-12-01 13:58:23 +0530

    UNSPSC interface added

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -10,25 +10,32 @@ export interface Product {
   data: unknown;
 }
 
-export interface UNSPSC {
+export interface BASE_UNSPSC_HELPER_TYPE {
   code: string;
-  description: string;
+  description: string | null;
   id?: number | string;
   is_deleted: boolean;
   name: string;
 }
 
-export interface Family extends UNSPSC {
+export interface UNSPSC extends BASE_UNSPSC_HELPER_TYPE {
+  segment_id: number;
+  family_id: number;
+  class_id: number;
+  commodity_id: number;
+}
+
+export interface Family extends BASE_UNSPSC_HELPER_TYPE {
   segment_id: number;
 }
 
-export interface Class extends UNSPSC {
+export interface Class extends BASE_UNSPSC_HELPER_TYPE {
   family_id: number;
 }
 
-export interface Commodity extends UNSPSC {
+export interface Commodity extends BASE_UNSPSC_HELPER_TYPE {
   class_id: number;
-  unspsc: string;
+  unspsc: UNSPSC;
 }
 
 // unspsc (segment, family, class, commodity),
