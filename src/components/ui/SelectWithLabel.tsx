3a7ea0fbf5090c333bd8f44f5d4e4a286bb9fa23
Code-X-Rahul <rr611305@gmail.com>
2025-01-24 02:56:50 +0530

    select ui fixes

--- src/components/ui/SelectWithLabel.tsx
+++ src/components/ui/SelectWithLabel.tsx
@@ -4,21 +4,27 @@ import {
   Option,
 } from "@/components/ui/ControlledComboBox";
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 
 interface Props<TData> extends ControlledComboBoxProps<TData> {
   labelText?: string;
   id?: string;
+  labelClassName?: string;
 }
 
 export default function SelectWithLabel<TData extends Option[]>({
   labelText,
   id,
+  labelClassName,
   ...rest
 }: Props<TData>) {
   return (
     <div className="grid gap-2">
       {labelText && (
-        <Label htmlFor={id}>
+        <Label
+          htmlFor={id}
+          className={cn("", labelClassName && labelClassName)}
+        >
           {labelText} {<span className="text-red-500">*</span>}
         </Label>
       )}
